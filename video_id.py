5e4a052e03c237dec1956e2b70f6adf7a2763ee4
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2024-12-07 19:39:12 +0900

    🔨 動画id出すやつ

---
+++ video_id.py
@@ -0,0 +1,8 @@
+import re
+
+def extract_video_id(url):
+    pattern = r"(?:v=|\/)([0-9A-Za-z_-]{11})"
+    match = re.search(pattern, url)
+    if match:
+        return match.group(1)
+    return None
